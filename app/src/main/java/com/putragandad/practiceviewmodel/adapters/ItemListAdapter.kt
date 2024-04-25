1d9cf8f46e4e9cdead78eed1783efa1284b6ef98
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2024-04-25 13:21:33 +0700

    Added Adapter for Card List

--- app/src/main/java/com/putragandad/practiceviewmodel/adapters/ItemListAdapter.kt
+++ app/src/main/java/com/putragandad/practiceviewmodel/adapters/ItemListAdapter.kt
@@ -1,4 +1,43 @@
 package com.putragandad.practiceviewmodel.adapters
 
-class ItemListAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.card.MaterialCardView
+import com.putragandad.practiceviewmodel.R
+import com.putragandad.practiceviewmodel.models.Item
+
+class ItemListAdapter(private val dataSet: ArrayList<Item>, private val onClickListener: OnClickListener): RecyclerView.Adapter<ItemListAdapter.ViewHolder>() {
+   class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+       val cardTitle = view.findViewById<TextView>(R.id.tv_list_card_title)
+       val cardDescription = view.findViewById<TextView>(R.id.tv_list_card_desc)
+   }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.rv_list_card, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val data = dataSet.get(position) // get data from the current position
+        holder.cardTitle.text = data.name
+        holder.cardDescription.text = data.description
+
+        val itemCard = holder.itemView.findViewById<MaterialCardView>(R.id.card_list)
+        itemCard.setOnClickListener {
+            onClickListener.onItemClicked(data)
+        }
+
+    }
+}
+
+interface OnClickListener {
+    fun onItemClicked(item: Item)
 }
